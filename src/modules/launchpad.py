11293dedb44cd67bc8ff2fd013c0c3a528d96b90
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-02 14:53:11 +0200

    basic structure of launchpad handler

--- src/modules/launchpad.py
+++ src/modules/launchpad.py
@@ -2,4 +2,14 @@
 
 The Launchpad handler.
 """
-import launchpad_py
+import launchpad_py as launchpad
+lp = launchpad.Launchpad()
+lp.Open()
+lp.Reset()
+
+# TODO:
+#   JSON Settings
+#   Button press: Start/Stop
+#   Button hold: Loop/Unloop
+#   Button double press: Enable/Disable (maybe?)
+
