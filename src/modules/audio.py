6d031a19c116feaa71aec69f3db90a12de9ed87c
Silvan Kohler <edu.silvan.kohler@gmail.com>
2020-06-06 22:01:08 +0200

    audio handler

--- src/modules/audio.py
+++ src/modules/audio.py
@@ -1,4 +1,16 @@
 from pydub import AudioSegment
+
+
 class Audio:
-    def __init__(self):
-        self.
+    def __init__(self, audiofile):
+        self.audiofile = audiofile
+        self.playing = False
+
+    def play(self):
+        self.playing = True
+        AudioSegment.from_file(self.audiofile)
+
+    def stop(self):
+        self.playing = False
+button1audio = Audio(path)
+button1audio.play()
