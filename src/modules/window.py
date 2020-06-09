db354ad582ae790f62456b6ceffb63ea083bf0e7
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-09 15:09:53 +0200

    added imports and tests

--- src/modules/window.py
+++ src/modules/window.py
@@ -5,19 +5,27 @@ The window handler.
 from sys import argv, exit
 
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget
 
 from modules.ui import Ui_window
 
 
 # TODO:
 #   Launchpad selection
+#   Button assignment
+#   Drag'n'Drop
+#   Volume apply
+#   Configfsiles
 
 class Window:
     def __init__(self, width, height):
         self.app = QApplication(argv)
+        self.window = QtWidgets.QMainWindow()
         self.widget = QWidget()
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
+        self.ui = Ui_window()
+        self.ui.setupUi(self.window)
 
     @property
     def height(self):
@@ -46,9 +54,7 @@ class Window:
         self.widget.resize(self.__width, self.__height)
 
 
-app = QtWidgets.QApplication(argv)
-window = QtWidgets.QMainWindow()
-ui = Ui_window()
-ui.setupUi(window)
-window.show()
-exit(app.exec_())
+if __name__ == "__main__":
+    w = Window(640, 480)
+    w.window.show()
+    exit(w.app.exec_())
