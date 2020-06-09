d55b6208508293e5e17dd731f671505cfa27ec9a
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-09 15:08:24 +0200

    added window and audio test

--- src/main.py
+++ src/main.py
@@ -1,3 +1,10 @@
-from modules import window
-from modules import audio
-from modules import launchpad
+from modules.audio import audiotest
+from modules.window import Window
+
+# Window initialisation
+w = Window(640, 480)
+w.window.show()
+# Audiotest
+audiotest()
+# Exit on command
+exit(w.app.exec_())
