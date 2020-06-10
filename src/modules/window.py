e6e3cdd3c193123c756ee47f6f683bdcec4e68aa
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-10 20:15:22 +0200

    updated size handling

--- src/modules/window.py
+++ src/modules/window.py
@@ -5,7 +5,7 @@ The window handler.
 from sys import argv, exit
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication
 
 from modules.ui import Ui_window
 
@@ -15,17 +15,21 @@ from modules.ui import Ui_window
 #   Button assignment
 #   Drag'n'Drop
 #   Volume apply
-#   Configfsiles
+#   Configfiles
 
 class Window:
     def __init__(self, width, height):
         self.app = QApplication(argv)
         self.window = QtWidgets.QMainWindow()
-        self.widget = QWidget()
-        self.__width = width
-        self.__height = height
         self.ui = Ui_window()
         self.ui.setupUi(self.window)
+        # print(self.ui.configbuttons.Open., self.ui.configbuttons.Reset, self.ui.configbuttons.Close, )
+        self.__width = width
+        self.__height = height
+        self.width = width
+        self.height = height
+        self.running = True
+        # self.handler = Thread(target=self.__handler, name="windowhandler")
 
     @property
     def height(self):
@@ -38,7 +42,7 @@ class Window:
     @height.setter
     def height(self, height):
         self.__height = height
-        self.widget.resize(self.__width, self.__height)
+        self.window.resize(self.__width, self.__height)
 
     @property
     def width(self):
@@ -51,10 +55,20 @@ class Window:
     @width.setter
     def width(self, width):
         self.__width = width
-        self.widget.resize(self.__width, self.__height)
+        self.window.resize(self.__width, self.__height)
+
+    # def __handler(self):
+    #     while True:
+    #         if self.running:
+    #             if self.ui.configbuttons.:
+    #                 print(self.ui.configfile.text())
+    #         else:
+    #             exit(1)
 
 
 if __name__ == "__main__":
     w = Window(640, 480)
     w.window.show()
+    # w.handler.start()
     exit(w.app.exec_())
+    w.running = False
