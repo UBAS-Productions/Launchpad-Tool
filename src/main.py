fdd971dae4d5a72d471a600c293c6ddd2f436019
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-10 20:13:45 +0200

    changed exitcode

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ from modules.window import Window
 # Window initialisation
 w = Window(640, 480)
 w.window.show()
+# w.handler.start()
 # Audiotest
 audiotest()
 # Exit on command
-exit(w.app.exec_())
+exitcode = w.app.exec_()
+# w.running = False
+exit(exitcode)
