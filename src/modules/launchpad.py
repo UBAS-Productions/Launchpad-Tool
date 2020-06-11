e2a7b13237beb8321e0e3e0e348351f87c4bf914
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-11 21:03:25 +0200

    added configfiles and launchpad discovery

--- src/modules/launchpad.py
+++ src/modules/launchpad.py
@@ -2,16 +2,71 @@
 
 The Launchpad handler.
 """
-import launchpad_py as launchpad
+from time import sleep
 
-try:
-    lp = launchpad.Launchpad()
-    lp.Open()
-    lp.Reset()
-except:
-    pass
+import launchpad_py as launchpad
+from pygame import midi
+
+
+def SearchDevices(name, output=True, input=True):
+    ret = []
+
+    for n in range(midi.get_count()):
+        md = midi.get_device_info(n)
+        if name.lower() in str(md[1]).lower():
+            if output is True and md[3] > 0:
+                ret.append(str(md[1], "UTF-8"))
+            if input is True and md[2] > 0:
+                ret.append(str(md[1], "UTF-8"))
+    return ret
+
+
 # TODO:
 #   JSON Settings
 #   Button press: Start/Stop
 #   Button hold: Loop/Unloop
 #   Button double press: Enable/Disable (maybe?)
+#   Window with time left for sound with activated TC
+class Launchpad:
+    def __init__(self):
+        self.lp = launchpad.Launchpad()
+        self.__config = {}
+        self.__launchpad = None
+
+    def open(self, lp):
+        self.lp.Open(lp)
+        self.reset()
+        self.lp.LedAllOn()
+        sleep(1)
+        self.reset()
+
+    def reset(self):
+        self.lp.Reset()
+
+    def close(self):
+        self.lp.Close()
+
+    @property
+    def launchpads(self):
+        return SearchDevices("Launch")
+
+    @property
+    def launchpad(self):
+        pass
+
+    @launchpad.setter
+    def launchpad(self, lp):
+        try:
+            if lp is not self.__launchpad:
+                self.__launchpad = lp
+                self.open(lp)
+        except:
+            pass
+
+    @property
+    def config(self):
+        return self.__config
+
+    @config.setter
+    def config(self, config):
+        self.__config = config
--- src/modules/window.py
+++ src/modules/window.py
@@ -2,6 +2,8 @@
 
 The window handler.
 """
+import json
+from os import path
 from sys import argv, exit
 
 from PyQt5 import QtWidgets
@@ -11,10 +13,12 @@ from modules.ui import Ui_window
 
 
 # TODO:
-#   Launchpad selection
+#   ✓ Launchpad selection
 #   Button assignment
-#   Drag'n'Drop
+#   ✓ Drag'n'Drop
 #   Volume apply
+#   Audiofilelist
+#   Active
 #   Configfiles
 
 class Window:
@@ -23,13 +27,69 @@ class Window:
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_window()
         self.ui.setupUi(self.window)
-        # print(self.ui.configbuttons.Open., self.ui.configbuttons.Reset, self.ui.configbuttons.Close, )
+        self.ui.launchpadselection.currentIndexChanged.connect(self.selectlaunchpad)
+        self.ui.configbuttons.buttons()[0].clicked.connect(self.saveconfig)
+        self.ui.configbuttons.buttons()[1].clicked.connect(self.openconfig)
+        self.ui.configbuttons.buttons()[2].clicked.connect(self.resetconfig)
+        self.config = {}
+        self.__button = 1
+        self.launchpad = None
+        self.changed = False
         self.__width = width
         self.__height = height
         self.width = width
         self.height = height
         self.running = True
         # self.handler = Thread(target=self.__handler, name="windowhandler")
+
+    def addaudio(self):
+        self.ui.audiofiles.addItem(
+            path.abspath(self.ui.addaudio.text().replace("file://", "").replace("\r", "").replace("\n", "")))
+
+    def selectlaunchpad(self):
+        self.launchpad = self.ui.launchpadselection.currentIndex()
+        self.changed = True
+
+    def openconfig(self):
+        try:
+            self.config = json.load(
+                open(
+                    path.abspath(
+                        self.ui.configfile.text().replace("file://", "").replace("\r", "").replace("\n", ""))))
+            self.changed = True
+        except:
+            pass
+
+    def saveconfig(self):
+        try:
+            json.dump(self.config, open(
+                path.abspath(self.ui.configfile.text().replace("file://", "").replace("\r", "").replace("\n", "")),
+                "w"),
+                      sort_keys=True)
+            self.changed = True
+        except:
+            pass
+
+    def resetconfig(self):
+        self.config = {}
+        self.changed = True
+
+    @property
+    def launchpads(self):
+        pass
+
+    @launchpads.setter
+    def launchpads(self, launchpads):
+        self.ui.launchpadselection.addItems(launchpads)
+
+    @property
+    def button(self):
+        return self.__button
+
+    @button.setter
+    def button(self, button):
+        self.__button = button
+        self.ui.buttonnumber.setText(f"Button {button}")
 
     @property
     def height(self):
@@ -64,7 +124,6 @@ class Window:
     #                 print(self.ui.configfile.text())
     #         else:
     #             exit(1)
-
 
 if __name__ == "__main__":
     w = Window(640, 480)
