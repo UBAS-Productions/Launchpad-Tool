678388d2e27136db8dc000327dac9bff26e256d7
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-11 21:04:49 +0200

    removed tests

--- src/modules/audio.py
+++ src/modules/audio.py
@@ -1,5 +1,3 @@
-import os
-
 from pydub import AudioSegment
 from pydub.playback import _play_with_simpleaudio as play
 from simpleaudio.functionchecks import LeftRightCheck
@@ -7,6 +5,8 @@ from simpleaudio.functionchecks import LeftRightCheck
 
 def audiotest():
     LeftRightCheck.run(0)
+
+
 class Audio:
     """
 
@@ -67,18 +67,3 @@ class Audio:
         print(self.src_audio.dBFS)
         self.audio = self.src_audio + ((self.src_audio.dBFS * (100 / volume)) - self.src_audio.dBFS)
         print(self.audio.dBFS)
-
-
-if __name__ == "__main__":
-    import time
-
-    path = os.path.join(os.getcwd(), "file.mp3")
-    print(path)
-    test = Audio(path)
-    test.play()
-    print(test.playing)
-    time.sleep(1000)
-    test.stop()
-
-# button1audio = Audio(path)
-# button1audio.play()
