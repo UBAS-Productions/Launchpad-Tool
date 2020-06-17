eb7adabc75812e46ee48ef92f68be6258faee7e3
SilvanKohler <edu.silvan.kohler@gmail.com>
2020-06-17 16:07:48 +0200

    faster editing replay option

--- src/modules/window.py
+++ src/modules/window.py
@@ -1,26 +1,24 @@
-"""
-
-The window handler.
-"""
 import json
 from os import path
 from sys import argv, exit
+from threading import Thread
 
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
+from pydub import AudioSegment
 
 from modules.ui import Ui_window
 
 
 # TODO:
 #   ✓ Launchpad selection
-#   Button assignment
+#   ✓ Button assignment
 #   ✓ Drag'n'Drop
-#   Volume apply
-#   Audiofilelist
-#   Activesettings
-#   Configfiles
+#   ✓ Volume apply
+#   ✓ Audiofilelist
+#   ✓ Activesettings
+#   ✓ Configfiles
 #   Buttonmatrix
 class Window:
     def __init__(self, width, height):
@@ -33,26 +31,72 @@ class Window:
         self.ui.configbuttons.buttons()[1].clicked.connect(self.openconfig)
         self.ui.configbuttons.buttons()[2].clicked.connect(self.resetconfig)
         self.ui.addbutton.clicked.connect(self.addaudio)
-        self.ui.volume.editingFinished.connect(self.edit)
+        self.ui.volume.valueChanged.connect(self.edit)
         self.ui.activated.clicked.connect(self.edit)
+        self.ui.replay.clicked.connect(self.edit)
+        self.ui.editmode.clicked.connect(self.__editmode)
         self.config = {}
         self.changed = False
         self.button = 16
         self.launchpad = None
+        self.editmode = False
         self.__width = width
         self.__height = height
         self.width = width
         self.height = height
         self.running = True
 
+    def __editmode(self):
+        self.editmode = self.ui.editmode.isChecked()
+
     def edit(self):
-        self.config.update({
-            int(self.ui.buttonnumber.text().replace("Button ", "")): [
-                path.abspath(self.ui.audiofile.text().replace("file://", "").replace("\r", "").replace("\n", "")),
-                self.ui.volume.value(),
-                self.ui.activated.isChecked()
-            ]
-        })
+        Thread(name="edit", target=self.__edit).start()
+
+    def __edit(self):
+        try:
+            # print(1)
+            if self.ui.audiofile.text().replace("file://", "").replace("\r", "").replace("\n", "") != "":
+                # print(2)
+                c = self.config.get(int(self.ui.buttonnumber.text().replace("Button ", "")), [""])
+                # print(3)
+                if c[0] != path.abspath(
+                        self.ui.audiofile.text().replace("file://", "").replace("\r", "").replace("\n", "")):
+                    # print(4)
+                    self.config.update({
+                        int(self.ui.buttonnumber.text().replace("Button ", "")): [
+                            path.abspath(
+                                self.ui.audiofile.text().replace("file://", "").replace("\r", "").replace("\n", "")),
+                            self.ui.volume.value(),
+                            self.ui.activated.isChecked(),
+                            self.ui.replay.isChecked(),
+                            AudioSegment.from_file(path.abspath(
+                                self.ui.audiofile.text().replace("file://", "").replace("\r", "").replace("\n", "")),
+                                path.abspath(
+                                    self.ui.audiofile.text().replace("file://", "").replace("\r",
+                                                                                            "").replace(
+                                        "\n", "")).split(".")[-1])
+                        ]
+                    })
+                    # print(5)
+                else:
+                    # print(6)
+                    self.config.update({
+                        int(self.ui.buttonnumber.text().replace("Button ", "")): [
+                            c[0],
+                            self.ui.volume.value(),
+                            self.ui.activated.isChecked(),
+                            self.ui.replay.isChecked(),
+                            c[4]
+                        ]
+                    })
+                    # print(7)
+            else:
+                # print(8)
+                del self.config[int(self.ui.buttonnumber.text().replace("Button ", ""))]
+            # print(9)
+        except:
+            pass
+        # print(self.config)
 
     def addaudio(self):
         file = path.abspath(self.ui.addaudio.text().replace("file://", "").replace("\r", "").replace("\n", ""))
@@ -65,15 +109,28 @@ class Window:
 
     def openconfig(self):
         try:
-            self.config = json.load(
-                open(
-                    path.abspath(
-                        self.ui.configfile.text().replace("file://", "").replace("\r", "").replace("\n", ""))))
+            tmp = {}
+            for key, value in json.load(
+                    open(
+                        path.abspath(
+                            self.ui.configfile.text().replace("file://", "").replace("\r", "").replace("\n",
+                                                                                                       "")))).items():
+                tmp.update({
+                    key: value.append(AudioSegment(value[0]))
+                })
+            self.config = tmp
+            del tmp
+            Thread(name="setbutton", target=self.setbutton, args=[self.button]).start()
         except:
             pass
 
     def saveconfig(self):
         try:
+            tmp = {}
+            for key, value in self.config:
+                tmp.update({
+                    key: value[:-1]
+                })
             json.dump(self.config, open(
                 path.abspath(self.ui.configfile.text().replace("file://", "").replace("\r", "").replace("\n", "")),
                 "w"),
@@ -83,6 +140,7 @@ class Window:
 
     def resetconfig(self):
         self.config = {}
+        Thread(name="setbutton", target=self.setbutton, args=[self.button])
 
     @property
     def launchpads(self):
@@ -96,17 +154,15 @@ class Window:
         if button is not None:
             self.button = button
             self.ui.buttonnumber.setText(f"Button {button}")
-            c = self.config.get(button, ["", 100.0, True])
-            self.ui.audiofile.setText(c[0])
-            self.ui.volume.setValue(c[1])
-            self.ui.activated.setChecked(c[2])
+            c = self.config.get(button, ["", 100.0, True, False])
+            # self.ui.audiofile.setText(c[0])
+            # self.ui.volume.setValue(c[1])
+            # self.ui.activated.setChecked(c[2])
+            # self.ui.replay.setChecked(c[3])
+            pass
 
     @property
     def height(self):
-        """
-
-        :return:
-        """
         return self.__height
 
     @height.setter
@@ -116,10 +172,6 @@ class Window:
 
     @property
     def width(self):
-        """
-
-        :return:
-        """
         return self.__width
 
     @width.setter
